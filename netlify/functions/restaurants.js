d2a67d574258cfa3e2a787a3e9ebe8066244dcc0
SreekarSBS <sbssreekar@gmail.com>
2025-06-06 16:41:45 +0530

    cors with netlify

--- netlify/functions/restaurants.js
+++ netlify/functions/restaurants.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 
 const SWIGGY_API_URL =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export async function handler(event, context) {
   try {
